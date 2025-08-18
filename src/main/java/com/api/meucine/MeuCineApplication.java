d8ab59c76e4902b7df970aaf2137a95d9586ad48
Cassius <rochadecassius@gmail.com>
2025-08-18 11:54:04 -0300

    Cria estrutura do projeto com Spring Initializr

---
+++ src/main/java/com/api/meucine/MeuCineApplication.java
@@ -0,0 +1,13 @@
+package com.api.meucine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/meucine/MeuCineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.meucine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
